c9c036109d3f270cd1c8884aae4fadd3ddee395b
mdcreator <63508140+mdcreator@users.noreply.github.com>
2021-04-09 01:04:47 +0300

    Initial commit

--- src/services/api.js
+++ src/services/api.js
@@ -24,21 +24,3 @@
 //   getImages,
 // };
 
-const API_KEY = '14395659-9e9c935ac58e7e08d55a10421';
-
-function fetchImage(query, page) {
-  return fetch(
-    `https://pixabay.com/api/?q=${query}&page=${page}&key=${API_KEY}&image_type=all&orientation=horizontal&per_page=12`,
-  ).then(response => {
-    if (response.ok) {
-      return response.json();
-    }
-    return Promise.reject(
-      new Error(`Нет такого изображения по запросу "${query}"`),
-    );
-  });
-}
-
-const api = { fetchImage };
-
-export default api;
